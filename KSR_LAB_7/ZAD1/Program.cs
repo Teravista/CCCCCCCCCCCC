4dc4e950939a455a3346d173a7179706100393dc
Teravista <chudy.robert660@gmail.com>
2024-05-07 02:28:28 +0200

    ksr_lab_7 mid

---
+++ KSR_LAB_7/ZAD1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ZAD1
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                UserName = "guest",
+                Password = "guest",
+                HostName = "localhost",
+                VirtualHost = "/",
+                Port = 5672
+            };
+
+            using (var connection = factory.CreateConnection()) 
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("message_queue", false, false, false, null);
+                var reply = channel.QueueDeclare().QueueName;
+                var consumer = new EventingBasicConsumer(channel);
+
+                var properties = channel.CreateBasicProperties();
+                properties.ReplyTo = reply;
+                var corrId = Guid.NewGuid().ToString();
+                properties.CorrelationId = corrId;
+                channel.BasicConsume(queue: reply, autoAck: true,consumer: consumer);
+
+                channel.BasicQos(0, 1, false);
+                for (int i = 0 ; i < 10; i++){
+                    string message = "my message number: " + i;
+                    var body = Encoding.UTF8.GetBytes(message);
+                    properties.Headers = new Dictionary<string, object>();
+                    properties.Headers.Add("header1", "value: "+i);
+                    properties.Headers.Add("header2", "value: "+(i+i));
+                    channel.BasicPublish("", "message_queue", properties, body);
+                }
+
+                Console.WriteLine("odpowiedz:-----------------------------");
+                consumer.Received += (model, ea) =>
+                {
+                    if (ea.BasicProperties.CorrelationId == corrId)
+                    {
+                        var body = ea.Body.ToArray();
+                        Console.WriteLine(Encoding.UTF8.GetString(body));
+                    }
+                };
+                Console.ReadKey();
+
+            }
+
+
+
+        }
+    }
+}
---
+++ KSR_LAB_7/ZAD2/Program.cs
@@ -0,0 +1,74 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZAD2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                UserName = "guest",
+                Password = "guest",
+                HostName = "localhost",
+                VirtualHost = "/",
+                Port = 5672
+            };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                queue: "message_queue",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+                );
+                channel.BasicQos(0, 1, false);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    string header1 = "-";
+                    string header2 = "-";
+                    if (ea.BasicProperties.Headers != null)
+                    {
+                        if (ea.BasicProperties.Headers.ContainsKey("header1"))
+                        {
+                            header1 = Encoding.UTF8.GetString((byte[])ea.BasicProperties.Headers["header1"]);
+                        }
+                        if (ea.BasicProperties.Headers.ContainsKey("header2"))
+                        {
+                            header2 = Encoding.UTF8.GetString((byte[])ea.BasicProperties.Headers["header2"]);
+                        }
+                    }
+
+
+
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body.ToArray());
+                    // show message
+                    Console.WriteLine("odebrana wiadomość:" + message);
+                    // var header1_message = Encoding.UTF8.GetString(BitConverter.GetBytes(header2));
+                    Console.WriteLine("z headerm 1:" + header1);
+
+                    //var header2_message = Encoding.UTF8.GetString(BitConverter.GetBytes(header2));
+                    Console.WriteLine("z headerm 2:" + header2);
+                    
+                     System.Threading.Thread.Sleep(2000);
+                    channel.BasicAck(ea.DeliveryTag, false);
+                    Console.WriteLine("wiadomość potwierdzona");
+                };
+                channel.BasicConsume(queue: "message_queue", autoAck: false, consumer: consumer);
+                Console.ReadKey();
+            }
+        }
+    }
+}
