7fecd8203be13d2ecb1282c5ce3cdeedcb8fa48b
Teravista <chudy.robert660@gmail.com>
2024-05-18 01:09:45 +0200

    KSR_10_finished_ihope

--- KSR_10/KlientA/KlientA.cs
+++ KSR_10/KlientA/KlientA.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Komunikaty;
+using MassTransit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,92 @@ namespace KlientA
 {
     internal class KlientA
     {
+        class HandlerAkceptacja : IConsumer<Komunikaty.AkceptacjaZamówienia>
+        {
+            public Task Consume(ConsumeContext<AkceptacjaZamówienia> ctx)
+            {
+                return Console.Out.WriteLineAsync("akceptacja zamowienia o ilosci: "+ctx.Message.ilosc);
+            }
+        }
+        class HandlerOdrzucenie : IConsumer<Komunikaty.OdrzucenieZamówienia>
+        {
+            public Task Consume(ConsumeContext<OdrzucenieZamówienia> ctx)
+            {
+                return Console.Out.WriteLineAsync("odrzucenie zamowienia o ilosci: " + ctx.Message.ilosc);
+            }
+        }
+        class HandlerPotwierdzenie : IConsumer<Komunikaty.PytanieOPotwierdzenie>
+        {
+            public Task Consume(ConsumeContext<PytanieOPotwierdzenie> ctx)
+            {
+                Console.WriteLine("potwerdzasz zamowienie ? {N/Y}");
+                while (true)
+                {
+                    ConsoleKeyInfo cki = Console.ReadKey();
+                    Console.WriteLine();
+                    switch (cki.Key)
+                    {
+                        case ConsoleKey.N:
+                            ctx.Publish(new Komunikaty.BrakPotwierdzenia() { CorrelationId = ctx.Message.CorrelationId });
+                            Console.WriteLine("Odrzucamy");
+                            return Console.Out.WriteLineAsync("");
+                        case ConsoleKey.Y:
+                            ctx.Publish(new Komunikaty.Potwierdzenie() { CorrelationId = ctx.Message.CorrelationId });
+                            Console.WriteLine("Potwierdzamy");
+                            return Console.Out.WriteLineAsync("");
+
+                    }
+                }
+            }
+        }
         static void Main(string[] args)
         {
+            var handlPotwierdzeniue = new HandlerPotwierdzenie();
+            var handlerOdrzucenie = new HandlerOdrzucenie();
+            var handlAkceptacja = new HandlerAkceptacja();
+            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+            {
+                var host = sbc.Host(new Uri("rabbitmq://localhost"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+                sbc.ReceiveEndpoint(host, "MainQueueA", ep => {
+                    ep.Instance(handlPotwierdzeniue);
+                    ep.Instance(handlerOdrzucenie);
+                    ep.Instance(handlAkceptacja);
+                });
+            });
+            bus.Start();
+            Console.WriteLine("KLIENT AAAA START");
+            ConsoleKeyInfo cki;
+            do
+            {
+                Console.WriteLine("type s to craete new saga");
+                cki = Console.ReadKey();
+                Console.WriteLine();
+                switch (cki.Key)
+                {
+                    case ConsoleKey.S:
+                        try
+                        {
+                            Console.WriteLine("Type number to create request");
+                            int intTemp = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("podaj Login zamowienia");
+                            string log = Console.ReadLine();
+                            bus.Publish(new Komunikaty.StartZamówienia() { ilosc = intTemp,login = log });
+                        }
+                        catch { }
+                        break;
+                    case ConsoleKey.T:
+                       
+                        break;
+
+                }
+
+            } while (cki.Key != ConsoleKey.Escape);
+            bus.Stop();
+
         }
     }
 }
--- KSR_10/Sklep/Sklep.cs
+++ KSR_10/Sklep/Sklep.cs
@@ -1,15 +1,155 @@
-﻿using System;
+﻿using Automatonymous;
+using Komunikaty;
+using MassTransit;
+using MassTransit.Saga;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
+using static MassTransit.MessageHeaders;
 
 namespace Sklep
 {
+    public class RejestracjaDane : SagaStateMachineInstance
+    {
+        public Guid CorrelationId { get; set; } // wymagane
+        public string CurrentState { get; set; } // wymagane
+        public string login { get; set; }
+        public int ilosc { get; set; }
+        public Guid? timeoutId
+        {
+            get;
+            set;
+        }
+    }
+    public class RejestracjaSaga : MassTransitStateMachine<RejestracjaDane>
+    {
+
+        // stany sagi poza początkowym
+        public State Niepotwierdzone { get; private set; }
+
+        public State PoteirdzonyKlient { get; private set; }
+        public State PoteirdzonyMagazyn { get; private set; }
+        // zdarzenia
+        public Event<Komunikaty.StartZamówienia> Rej { get; private set; }
+        public Event<Komunikaty.Potwierdzenie> Potw { get; private set; }
+        public Event<Komunikaty.BrakPotwierdzenia> BrakPotw { get; private set; }
+        public Event<Komunikaty.OdpowiedzWolne> Wolne { get; private set; }
+        public Event<Komunikaty.ENDORADO> odpENDER { get; private set; }
+        public Event<Komunikaty.OdpowiedzWolneNegatywna> WolneNega { get; private set; }
+
+        public Event<Komunikaty.Timeout> TimeoutEvt { get; private set; }
+        public Schedule<RejestracjaDane, Komunikaty.Timeout> TO
+        {
+            get; private set;
+        }
+
+        public RejestracjaSaga()
+        {
+            //timeouter
+            Schedule(() => TO, x => x.timeoutId, x => { x.Delay = TimeSpan.FromSeconds(1); });
+            DuringAny(When(TimeoutEvt).Then(ctx =>
+            {
+                //Console.WriteLine("TIMEOUTTTTTTTTTTTTTTTTTTT");
+                ctx.Publish(new Komunikaty.ENDORADO() { CorrelationId = ctx.Instance.CorrelationId });
+            }));
+            InstanceState(x => x.CurrentState);
+            //2
+            Event(() => Rej, x => x.CorrelateBy(s => s.login, ctx => ctx.Message.login).SelectId(context => Guid.NewGuid()));
+            //3
+            Initially(
+                When(Rej).Then(context => { }).ThenAsync(ctx =>
+                {
+                    ctx.Publish(new Komunikaty.PytanieOPotwierdzenie() { ilosc = ctx.Data.ilosc, CorrelationId = ctx.Instance.CorrelationId });
+                    ctx.Publish(new Komunikaty.PytanieoWolne() { ilosc = ctx.Data.ilosc, CorrelationId = ctx.Instance.CorrelationId });
+                    ctx.Instance.ilosc = ctx.Data.ilosc;
+                    return Console.Out.WriteLineAsync(
+                        $"ilosc={ctx.Data.ilosc}   " +
+                        $"id={ctx.Instance.CorrelationId}");
+                }).Schedule(TO, ctx => new Komunikaty.Timeout() { CorrelationId = ctx.Instance.CorrelationId })
+                // .Respond(ctx =>
+                //{
+                // return// new Komunikaty.PytanieOPotwierdzenie()
+                //  { id = ctx.Instance.CorrelationId };
+                // })
+                .TransitionTo(Niepotwierdzone));
+            //4
+            During(Niepotwierdzone, When(Potw).Then(ctx => { }).ThenAsync(ctx => { return Console.Out.WriteLineAsync("Akecptacjaużytkownika"); }).TransitionTo(PoteirdzonyKlient));
+            During(Niepotwierdzone, When(Wolne).Then(ctx => { }).ThenAsync(ctx => { return Console.Out.WriteLineAsync("wystarczajaco rzeczy w magazynie"); }).TransitionTo(PoteirdzonyMagazyn));
+
+            //
+            During(PoteirdzonyKlient, When(Wolne).Then(ctx => { }).ThenAsync(ctx =>
+            {
+                Console.Out.WriteLineAsync("warunki spelnione Zamówienie spełnione");
+                ctx.Publish(new Komunikaty.AkceptacjaZamówienia() { ilosc = ctx.Instance.ilosc });
+                return Console.Out.WriteLineAsync("");
+            }).Finalize());
+
+            During(PoteirdzonyMagazyn, When(Potw).Then(ctx => { }).ThenAsync(ctx =>
+            {
+                Console.Out.WriteLineAsync("warunki spelnione Zamówienie spełnione");
+                ctx.Publish(new Komunikaty.AkceptacjaZamówienia() { ilosc = ctx.Instance.ilosc });
+                return Console.Out.WriteLineAsync("");
+            }).Finalize());
+
+
+
+            //
+            DuringAny(When(WolneNega).Then(ctx =>
+            {
+                Console.WriteLine("koniec Brak wolnega magazynu");
+                ctx.Publish(new Komunikaty.OdrzucenieZamówienia() { ilosc = ctx.Instance.ilosc, czyDoZwrotu = false });
+            }).Finalize());
+            ///ednordao
+            DuringAny(When(odpENDER).Then(ctx =>
+            {
+                Console.WriteLine("TIMED OUT");
+                ctx.Publish(new Komunikaty.OdrzucenieZamówienia() { ilosc = ctx.Instance.ilosc, czyDoZwrotu = false });
+            }).Finalize());
+
+            During(PoteirdzonyMagazyn, When(BrakPotw).Then(ctx =>
+            {
+                Console.WriteLine("odmowa klienta + zwracenie magazynu");
+                ctx.Publish(new Komunikaty.OdrzucenieZamówienia() { ilosc = ctx.Instance.ilosc, czyDoZwrotu = true });
+            }).Finalize());
+            During(Niepotwierdzone, When(BrakPotw).Then(ctx =>
+            {
+                Console.WriteLine("odmowa klienta");
+                ctx.Publish(new Komunikaty.OdrzucenieZamówienia() { ilosc = ctx.Instance.ilosc, czyDoZwrotu = false });
+            }).Finalize());
+            //6
+            DuringAny(When(Rej).Then(ctx => { }));
+            //5
+            SetCompletedWhenFinalized();
+        }
+
+    }
+
     internal class Sklep
     {
         static void Main(string[] args)
         {
+            var repo = new InMemorySagaRepository<RejestracjaDane>();
+            var machine = new RejestracjaSaga();
+            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+            {
+                var host = sbc.Host(new Uri("rabbitmq://localhost"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+                sbc.ReceiveEndpoint(host, "MainQueueS", ep =>
+                {
+                    ep.StateMachineSaga(machine, repo);
+                });
+                sbc.UseInMemoryScheduler();
+            });
+            bus.Start();
+            Console.WriteLine("SKLEP START!!");
+            Console.ReadKey();
+            bus.Stop();
         }
     }
 }
