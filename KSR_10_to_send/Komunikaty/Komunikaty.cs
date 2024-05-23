08a4a479f08cde6ae32b86702a76c0d298143f15
Teravista <chudy.robert660@gmail.com>
2024-05-23 12:35:33 +0200

    KSR UPLOAD

---
+++ KSR_10_to_send/Komunikaty/Komunikaty.cs
@@ -0,0 +1,26 @@
+﻿using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komunikaty
+{
+    //public class Rejestracja { public string login;  }
+    public class StartZamówienia { public int ilosc; public string login;public string sender; }
+    public class PytanieOPotwierdzenie : CorrelatedBy<Guid> { public int ilosc; public Guid CorrelationId { get; set; } }
+    public class Potwierdzenie : CorrelatedBy<Guid> { public string login; public Guid CorrelationId { get; set; } }
+    public class BrakPotwierdzenia : CorrelatedBy<Guid> { public Guid CorrelationId { get; set; } }
+    public class PytanieoWolne : CorrelatedBy<Guid> { public int ilosc; public Guid CorrelationId { get; set; } }
+    public class OdpowiedzWolne : CorrelatedBy<Guid> { public Guid CorrelationId { get; set; } }
+    public class ENDORADO : CorrelatedBy<Guid> { public Guid CorrelationId { get; set; } }
+    public class OdpowiedzWolneNegatywna : CorrelatedBy<Guid> { public Guid CorrelationId { get; set; } }
+    public class AkceptacjaZamówienia : CorrelatedBy<Guid> { public int ilosc; public Guid CorrelationId { get; set; } }
+    public class OdrzucenieZamówienia : CorrelatedBy<Guid> { public int ilosc;public bool czyDoZwrotu; public Guid CorrelationId { get; set; } }
+    public class Timeout : CorrelatedBy<Guid>
+    {  public Guid CorrelationId { get; set; }
+    }
+
+}
---
+++ KSR_10_to_send/Magazyn/Magazyn.cs
@@ -0,0 +1,136 @@
+﻿using Komunikaty;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magazyn
+{
+
+    internal class Magazyn
+    {
+        class MagazynStan
+        {
+            public int wolne = 0;
+            public int zarezerwowane = 0;
+        }
+
+        class HandlerAkceptacja : IConsumer<Komunikaty.AkceptacjaZamówienia>
+        {
+            public MagazynStan magStan;
+            public HandlerAkceptacja(MagazynStan magStanRef)
+            {
+                this.magStan = magStanRef;
+            }
+            public Task Consume(ConsumeContext<AkceptacjaZamówienia> ctx)
+            {
+                this.magStan.zarezerwowane -= ctx.Message.ilosc;
+                return Console.Out.WriteLineAsync("akceptacja zamowienia o ilosci: " + ctx.Message.ilosc);
+            }
+        }
+        class HandlerOdrzucenie : IConsumer<Komunikaty.OdrzucenieZamówienia>
+        {
+            public MagazynStan magStan;
+            public HandlerOdrzucenie(MagazynStan magStanRef)
+            {
+                this.magStan = magStanRef;
+            }
+            public Task Consume(ConsumeContext<OdrzucenieZamówienia> ctx)
+            {
+                if (ctx.Message.czyDoZwrotu)
+                {
+                    this.magStan.wolne += ctx.Message.ilosc;
+                    this.magStan.zarezerwowane -= ctx.Message.ilosc;
+                }
+                return Console.Out.WriteLineAsync("odrzucenie zamowienia o ilosci: " + ctx.Message.ilosc);
+            }
+        }
+        class HandlerClassStats : IConsumer<Komunikaty.PytanieoWolne>
+        {
+            public MagazynStan magStan;
+            public HandlerClassStats(MagazynStan magStanRef)
+            {
+                this.magStan = magStanRef;
+            }
+            
+            public Task Consume(ConsumeContext<Komunikaty.PytanieoWolne> ctx)
+            {
+                Console.Out.WriteLineAsync("wolne: " + (this.magStan.wolne) + " zapotrzebowane: " + ctx.Message.ilosc);
+
+                if (this.magStan.wolne >= ctx.Message.ilosc)
+                {
+                    Console.Out.WriteLineAsync("Wystarczająco");
+                    this.magStan.wolne -= ctx.Message.ilosc;
+                    this.magStan.zarezerwowane += ctx.Message.ilosc;
+                    ctx.Publish(new Komunikaty.OdpowiedzWolne() { CorrelationId = ctx.Message.CorrelationId });
+
+                }
+                else
+                {
+                    Console.Out.WriteLineAsync("Nie Wystarczajaco");
+                    ctx.Publish(new Komunikaty.OdpowiedzWolneNegatywna() { CorrelationId = ctx.Message.CorrelationId });
+
+                }
+                return Console.Out.WriteLineAsync("");
+            }
+        }
+        static void Main(string[] args)
+        {
+            var magazyn = new MagazynStan();
+            var hanldAkceptacja = new HandlerAkceptacja(magazyn);
+            var hanldOdrzucenie = new HandlerOdrzucenie(magazyn);
+            var hanlderClass = new HandlerClassStats(magazyn);
+            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+            {
+                var host = sbc.Host(new Uri("rabbitmq://localhost"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+                sbc.ReceiveEndpoint(host, "MainQueueM", ep =>
+                {
+                    ep.Instance(hanlderClass);
+                    ep.Instance(hanldOdrzucenie);
+                    ep.Instance(hanldAkceptacja);
+                });
+            });
+            bus.Start();
+            Console.WriteLine("Stan Magazynu Wolne: 0");
+            Console.WriteLine("Stan Magazynu Zarezerwowane: 0");
+            ConsoleKeyInfo cki;
+            do
+            {
+                Console.WriteLine("type a to add items");
+                cki = Console.ReadKey();
+                Console.WriteLine();
+                switch (cki.Key)
+                {
+                    case ConsoleKey.A:
+                        try
+                        {
+                            Console.WriteLine("Type number to add");
+                            int intTemp = Convert.ToInt32(Console.ReadLine());
+                            magazyn.wolne += intTemp;
+                            Console.Clear();
+                            Console.WriteLine("Stan Magazynu Wolnego: " + magazyn.wolne);
+                            Console.WriteLine("Stan Magazynu Zarezerwowane: "+magazyn.zarezerwowane);
+
+                        }
+                        catch { }
+                        break;
+                    case ConsoleKey.C:
+                        Console.Clear();
+                        Console.WriteLine("Stan Magazynu Wolnego: " + magazyn.wolne);
+                        Console.WriteLine("Stan Magazynu Zarezerwowane: " + magazyn.zarezerwowane);
+                        break;
+
+                }
+
+            } while (cki.Key != ConsoleKey.Escape);
+            bus.Stop();
+
+        }
+    }
+}
