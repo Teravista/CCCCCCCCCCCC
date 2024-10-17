a39753c8773dc2fd5506af9cb52628e69036cdb8
Teravista <chudy.robert660@gmail.com>
2024-10-17 09:28:13 +0200

    lab

---
+++ tester/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using OfficeOpenXml;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            var ep = new ExcelPackage(new FileInfo(@"C:\Users\Robert\source\repos\tester\ConsoleApp1\lab3.xlsx"));
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
